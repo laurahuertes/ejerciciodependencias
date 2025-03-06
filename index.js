c0072d61ec2fc7169bd0dbb41f685eb00d5bb3bd
laurahuertes <laura.huertes@babelgroup.com>
2025-03-06 12:21:08 +0100

    Añadiendo lodash y axios en dependencias

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const axios = require("axios");
+const _ = require("lodash"); // Añadir la dependencia lodash
 
 async function obtenerDatos() {
   try {
     const respuesta = await axios.get(
       "https://jsonplaceholder.typicode.com/todos/1"
     );
-    console.log("Datos obtenidos:", respuesta.data);
+    const datos = respuesta.data;
+
+    // Usar lodash para clonar los datos obtenidos
+    const datosClonados = _.cloneDeep(datos);
+
+    console.log("Datos obtenidos:", datos);
+    console.log("Datos clonados:", datosClonados);
   } catch (error) {
     console.error("Error al obtener los datos:", error.message);
   }
